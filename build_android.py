ad654dbecce5d01f59a1a3b533f8e64a1eb2604e
Starbqn <eveeteteetteedae@gmail.com>
2025-03-27 06:24:56 +0000

    adding mobile

--- build_android.py
+++ build_android.py
@@ -367,12 +367,22 @@ dependencies {
     implementation 'androidx.appcompat:appcompat:1.6.1'
     implementation 'com.google.android.material:material:1.9.0'
     implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+    implementation 'androidx.recyclerview:recyclerview:1.3.2'
+    implementation 'androidx.cardview:cardview:1.0.0'
+    implementation 'androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2'
+    implementation 'androidx.lifecycle:lifecycle-livedata-ktx:2.6.2'
+    implementation 'androidx.navigation:navigation-fragment-ktx:2.7.0'
+    implementation 'androidx.navigation:navigation-ui-ktx:2.7.0'
     testImplementation 'junit:junit:4.13.2'
     androidTestImplementation 'androidx.test.ext:junit:1.1.5'
     androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
     
     // For memory access (root devices only)
     implementation 'eu.chainfire:libsuperuser:1.1.1'
+    
+    // Shizuku API for elevated permissions without full root
+    implementation 'dev.rikka.shizuku:api:13.1.4'
+    implementation 'dev.rikka.shizuku:provider:13.1.4'
 }
 """
     
